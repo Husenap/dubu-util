bff630de72feb0a8cbc469868e6568bad5517f29
Hussein Taher <hussein_taher@outlook.com>
2020-10-08 18:20:47 +0200

    implemented TypeId

---
+++ dubu_util/src/dubu_util/type/TypeId.h
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace dubu::util {
+
+using IdType = uint16_t;
+
+class TypeId {
+public:
+	template <typename T>
+	static IdType Get() {
+		static IdType id = ++GlobalCounter;
+		return id;
+	}
+
+	template <typename T>
+	static IdType Get(const T&) {
+		return Get<T>();
+	}
+
+private:
+	static IdType GlobalCounter;
+};
+
+}  // namespace dubu::util
